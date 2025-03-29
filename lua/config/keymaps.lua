a1b381ef180b11a79aeba132e638deee501ac29d
Jonathan <jonathan.leimbach2408@gmail.com>
2025-03-29 13:19:51 +0100

    updated config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Save file and quit
+keymap.set("n", "<Leader>w", ":update<Return>", opts)
+keymap.set("n", "<Leader>q", ":quit<Return>", opts)
+keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- resize window
+vim.keymap.set("n", "<C-Left>", "<C-w><", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Right>", "<C-w>>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>", "<C-w>+", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>", "<C-w>-", { noremap = true, silent = true })
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.lsp.diagnostic.goto_next()
+end, opts)
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,29 @@
 return {
+  --gruvbox - the goat
   { "ellisonleao/gruvbox.nvim" },
+  --kanagawa
   { "rebelot/kanagawa.nvim" },
+  --rose-pine
   { "rose-pine/nvim", name = "rose-pine" },
+  --everforest
   { "sainnhe/everforest" },
+  --Apprentice
   { "romainl/Apprentice" },
+  --onedark
+  { "olimorris/onedarkpro.nvim", name = "onedark_vivid" },
+  { "olimorris/onedarkpro.nvim", name = "onedark" },
+  { "olimorris/onedarkpro.nvim", name = "onedark_dark" },
+  --tokyodark
+  { "tiagovla/tokyodark.nvim" },
 
-  -- configure LazyVim to load colorscheme
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
+    "sainnhe/sonokai",
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = 1
+      vim.g.sonokai_style = "andromeda"
+      vim.cmd.colorscheme("sonokai")
+    end,
   },
 }
